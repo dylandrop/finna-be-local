c18e2914f1960b1ac94004631e29687d6a9be126
Dylan <dylandrop@gmail.com>
2013-01-31 16:53:27 -0500

    added listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,13 +1,18 @@
 class ListingsController < ApplicationController
   def new
+    @listing = Listing.new
   end
 
   def index
+    @listings = Listing.all
   end
 
   def show
+    @listing = Listing.find(params[:id])
   end
 
   def create
+    @listing = Listing.new(params[:listing])
+    @listing.save
   end
 end
