daa949083d8d086a2ad5768b3924fa001c02f671
Dylan <dylandrop@gmail.com>
2013-01-31 15:46:52 -0500

    added rails proj

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FinnaBeLocal::Application.config.secret_token = '9d3f4051edf4bb8021de2790e5697236ecffb85a16067675bc71e4fdaa9f9ee2ed295fcb5adb419e3d9a43941c6546d231d15a289c60fb5d2ff2a1440f85c11b'
