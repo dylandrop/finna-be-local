6f5096fec5cf98fc3f785145bb9b1102bed43611
Dylan <dylandrop@gmail.com>
2013-02-27 16:17:07 -0500

    full text search added

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,11 +1,15 @@
 class Listing < ActiveRecord::Base
+  include PgSearch
+
   attr_accessible :body, :name, :seller_email, :zip, :tag_ids
   has_many :taggings
   has_many :tags, through: :taggings
 
+  pg_search_scope :search_by_name, against: :name, using: { tsearch: { any_word: true }} 
+
   scope :named_like, (lambda do |name| 
     if name.present?
-      self.where("listings.name ILIKE ?", "%#{name}%")
+      self.search_by_name(name)
     end
   end)
 
